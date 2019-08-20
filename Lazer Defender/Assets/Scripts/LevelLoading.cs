a9e9ac32bd4969016fb5d377ff5fdf92fec66bad
Brenden Hover <Brenden_hoverXD@live.com>
2019-08-20 17:58:07 -0500

    Full game
    Commit after the game was completed, because I didn't have this stuff set up when I made it

---
+++ Lazer Defender/Assets/Scripts/LevelLoading.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoading : MonoBehaviour {
+
+    [SerializeField] float GameOverDelay = 2f;
+
+    public void LoadStartMenu() {
+        SceneManager.LoadScene(0); // Must change if the start menu is no longer the first scene!
+    }
+
+    public void LoadGame() {
+        SceneManager.LoadScene("Main Game"); // This also works, but it's not usually a great idea
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+
+    public void LoadGameOver() {
+        StartCoroutine(DelayGameOver());
+    }
+
+    IEnumerator DelayGameOver() {
+        yield return new WaitForSeconds(GameOverDelay);
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+}
